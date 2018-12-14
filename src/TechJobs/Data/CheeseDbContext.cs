f7948ba468fd9dbc49997e566d3664ba6aae198a
Jennifer Flores <jlflores@charter.net>
2018-12-13 19:37:17 -0600

    initial push

---
+++ src/TechJobs/Data/CheeseDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TechJobs.Data
+{
+    public class CheeseDbContext
+    }
+        //public DbSet<CheeseCategory> Categories{get; set}
+    }
+}
---
+++ src/TechJobs/Models/Cheesecatagory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TechJobs.Models
+{
+    public class Cheesecatagory
+    {
+        //IDictionary Property
+        //public string = Name
+    }
+}
